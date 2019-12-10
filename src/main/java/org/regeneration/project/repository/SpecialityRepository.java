68c696c5d58027bfc4e38140fa8eeaba10673cfb
George Kostopoulos <George.Kst@icloud.com>
2019-12-10 18:50:53 +0200

    Delete redundant folders

--- src/main/java/org/regeneration/project/repository/SpecialityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.regeneration.project.repository;
-
-import org.regeneration.project.models.Speciality;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
-}
