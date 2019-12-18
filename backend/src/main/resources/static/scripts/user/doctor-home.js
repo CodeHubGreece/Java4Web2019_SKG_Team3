861b8d4521f3622d17e42733c1d3677422607292
Anastasios Margaritis <anastasismargaritis@gmail.com>
2019-12-18 19:27:49 +0200

    Corrections Doctor Page

--- backend/src/main/resources/static/scripts/user/doctor-home.js
+++ backend/src/main/resources/static/scripts/user/doctor-home.js
@@ -1,11 +1,10 @@
 $('document').ready(function(){
     const currentUser = JSON.parse(localStorage.getItem(LOCAL_STORAGE_USER));
     const userDetailsParagraphs = document.querySelectorAll("#userDetails > p");
-    userDetailsParagraphs[0].innerHTML = userDetailsParagraphs[0].innerHTML + " " + currentUser.firstName;
-    userDetailsParagraphs[1].innerHTML = userDetailsParagraphs[1].innerHTML + " " + currentUser.lastName;
-    userDetailsParagraphs[2].innerHTML = userDetailsParagraphs[2].innerHTML + " " + currentUser.username;
-    userDetailsParagraphs[3].innerHTML = userDetailsParagraphs[3].innerHTML + " " + currentUser.doctorSpecialityDto["specialityName"];
-
+    userDetailsParagraphs[0].innerHTML = userDetailsParagraphs[0].innerHTML + " " + currentUser.userFirstName;
+    userDetailsParagraphs[1].innerHTML = userDetailsParagraphs[1].innerHTML + " " + currentUser.userLastName;
+    userDetailsParagraphs[2].innerHTML = userDetailsParagraphs[2].innerHTML + " " + currentUser.userUsername;
+    userDetailsParagraphs[3].innerHTML = userDetailsParagraphs[3].innerHTML + " " + currentUser.doctorSpecialityDto.specialityName;
 
     const tableBody = document.querySelector("#table-body");
     const appointments = currentUser.doctorAppointmentDto;
@@ -28,14 +27,12 @@ $('document').ready(function(){
         let tableRow = document.createElement("tr");
 
         tableRow.addEventListener("click", function () {
-            console.log(appointment);
             $.ajax({
                 url: ROOT_PATH + '/citizen/' + appointment.citizenId,
                 processData: false,
                 contentType: false,
                 type: 'GET',
                 success: function(data) {
-                    console.log(data);
                     document.querySelector('.modal').classList.add('is-active');
                     const userDetailsParagraphs = document.querySelectorAll("#user-details > p");
                     userDetailsParagraphs[0].innerHTML = userDetailsParagraphs[0].innerHTML + " " + data.userFirstName;
@@ -91,61 +88,5 @@ $('document').ready(function(){
 
         tableBody.appendChild(tableRow);
     });
-    $.ajax({
-        url: ROOT_PATH + '/speciality',
-        processData: false,
-        contentType: false,
-        type: 'GET',
-        success: function(data) {
-            const specialitySelector = document.querySelector("#speciality");
-            const option = document.createElement("option");
-            option.text = "Choose a speciality";
-            option.value = "";
-            specialitySelector.appendChild(option);
-            data.map(function(speciality) {
-                const option = document.createElement("option");
-                option.text = speciality.name;
-                option.value = speciality.id;
-                specialitySelector.appendChild(option);
-            });
-        },
-        error: function(error) {
-            console.error("We had an error!");
-        }
-    });
+
 });
-
-function getDoctorsBySpeciality() {
-    const specialityId = document.querySelector("#speciality").value;
-    $.ajax({
-        url: ROOT_PATH + '/doctors/speciality/' + specialityId,
-        processData: false,
-        contentType: false,
-        type: 'GET',
-        success: function(data) {
-            const doctorSelector = document.querySelector(".doctor-selector");
-            doctorSelector.setAttribute("style", "visibility: visible;");
-            const specialitySelector = document.querySelector("#doctor");
-            const option = document.createElement("option");
-            option.text = "Choose a doctor";
-            option.value = "";
-            specialitySelector.appendChild(option);
-            data.map(function(doctor) {
-                const option = document.createElement("option");
-                option.text = doctor.userFirstName + " " + doctor.userLastName;
-                option.value = doctor.doctorId;
-                specialitySelector.appendChild(option);
-            });
-        },
-        error: function(error) {
-            console.error("We had an error!");
-        }
-    });
-}
-
-function showFields() {
-    const fields = document.querySelectorAll(".other-selectors");
-    fields.forEach(function(field) {
-        field.setAttribute("style", "visibility: visible;");
-    });
-}
